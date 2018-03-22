284507ed45b8b23be99d5551c8c89372dad80ce9
Faye Voulivasi <voulivat@auth.gr>
2018-03-22 22:05:05 +0800

    Add lexicon creation functionality

--- src/lexicon_creation.py
+++ src/lexicon_creation.py
@@ -1,6 +1,21 @@
+from collections import Counter
+from operator import itemgetter
+
 class LexiconCreator(object):
 	"""Create a lexicon using a custom corpus."""
+	
+	@classmethod	
+	def create(cls, base_corpus):
+		frequencies = Counter(base_corpus)
 		
+		sorted_words = sorted(frequencies.items(), reverse=True, key=itemgetter(1))
+		sorted_words = [w[0] for w in sorted_words]
+		
+		cls.write_to_file(sorted_words)
+
+	# TODO store "lexicon.txt" in appropriate location	
 	@staticmethod
-	def create(base_corpus):
-		pass
+	def write_to_file(words, filename="lexicon.txt"):
+	    with open (filename, "w") as f:
+	        for w in words:
+	            f.write("%s\n" % w)    
